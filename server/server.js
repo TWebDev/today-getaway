4485ce8fe6f33b8820b86c52ff42ce2839f6511c
Kauyumari <emiliano.jgj97@gmail.com>
2019-07-04 16:09:22 -0500

    Resueltas las direcciones client-rendering

--- server/server.js
+++ server/server.js
@@ -3,15 +3,17 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
-app.use(express.static(path.join('.', 'build')));
+var dir = __dirname + "/../";
+
+app.use(express.static(dir));
+app.use(express.static(path.join(dir, 'build')));
 
 app.get('/ping', function (req, res) {
   return res.send('pong');
 });
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join('.', 'build', 'index.html'));
+  res.sendFile(path.join(dir, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
