6a9385a219343567b68922b9535dabbcc0b9d827
TWebDev <52970731+TWebDev@users.noreply.github.com>
2019-07-22 11:39:01 -0500

    Cambio de formulario de landing page
    Se actualizó desde mailchimp

--- src/app/components/form/Form.js
+++ src/app/components/form/Form.js
@@ -131,72 +131,60 @@ const Form = (props) => {
           <h1>
             Become a checker today!
           </h1>     
-        </div>     
+        </div>
+        <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css" />     
         <div id="mc_embed_signup">
-          <form 
-            action=""  
-            method="post"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            className="validate"
-            target="_blank"
-            noValidate>
-            <div className="mc-field-group">
-              <Input        
-              Type="text"
-              Name="NAME"
-              Placeholder="First Name"
-              inputHandler={e => updateForm(e)}
-              Value={form.formFields.name}      
-              ></Input>
+          <form action="https://gmail.us20.list-manage.com/subscribe/post?u=626a961a7c5c37bc6daae9f82&amp;id=0239fd4541" 
+            method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" 
+            target="_blank" novalidate>
+              <div id="mc_embed_signup_scroll">
+                <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                <div class="mc-field-group">
+                  <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
+                  <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL"/>
+                </div>
+                <div class="mc-field-group">
+                  <label for="mce-FNAME">First Name </label>
+                  <input type="text" value="" name="FNAME" class="" id="mce-FNAME"/>
+                </div>
+                <div class="mc-field-group">
+                  <label for="mce-LNAME">Last Name </label>
+                  <input type="text" value="" name="LNAME" class="" id="mce-LNAME"/>
+                </div>
+                <div class="mc-field-group size1of2">
+                  <label for="mce-MMERGE6">Phone Number  <span class="asterisk">*</span>
+                </label>
+                  <input type="text" name="MMERGE6" class="required" value="" id="mce-MMERGE6"/>
+                </div>
+                <div class="mc-field-group">
+                  <label for="mce-MMERGE3">What is the best time to call you?  <span class="asterisk">*</span></label>
+                  <select name="MMERGE3" class="required" id="mce-MMERGE3">
+                    <option value=""></option>
+                    <option value="9:00 am - 12:00 pm">9:00 am - 12:00 pm</option>
+                    <option value="12:00 pm - 3:00 pm">12:00 pm - 3:00 pm</option>
+                    <option value="3:00 pm - 6:00 pm">3:00 pm - 6:00 pm</option>              
+                  </select>
+                </div>
+              <div class="mc-field-group">
+                <label for="mce-MMERGE4">When are you coming to Mexico?  <span class="asterisk">*</span></label>
+                <select name="MMERGE4" class="required" id="mce-MMERGE4">
+                  <option value=""></option>
+                  <option value="January - April">January - April</option>
+                  <option value="May - August">May - August</option>
+                  <option value="September - December">September - December</option>
+                </select>
+              </div>
+              <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+              </div>
+              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_626a961a7c5c37bc6daae9f82_0239fd4541" tabindex="-1" value=""/></div>
+              <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/></div>
             </div>
-            <div className="mc-field-group">
-              <Input        
-              Type="text"
-              Name="LAST"
-              Placeholder="Last Name"
-              inputHandler={e => updateForm(e)}
-              Value={form.formFields.last}            
-              ></Input>
-            </div>
-            <div className="mc-field-group">
-              <Input        
-              Type="tel"
-              Name="PHONE"
-              Placeholder="123-456-7890"
-              inputHandler={e => updateForm(e)}  
-              Value={form.formFields.phone}          
-              ></Input>
-            </div>
-            <div className="mc-field-group" id="email">
-              <Input        
-              Type="email"
-              Name="EMAIL"
-              Placeholder="email@mailme.com"
-              inputHandler={e => updateForm(e)}
-              Value={form.formFields.email}           
-            ></Input>
-            </div>    
-            <div className="mc-field-group" id="selects">
-              <Select
-              Options={["What's the best time to call you?","9:00 am - 12:00 pm","12:00 pm - 3:00 pm","3:00 pm - 6:00 pm"]}
-              Name="CALL_AT"
-              ></Select>
-              <Select
-              Options={["When are you coming to Mexico?","January - April","May - August","September - December"]}
-              Name="PREF_DATES"
-              ></Select>
-            </div>                                    
-            <Button
-              Type="submit"
-              Value="Subscribe"
-              _Id="mc-embedded-subscribe"
-              disabled={disabled}
-            >
-              Begin your experience
-            </Button>          
           </form>
         </div>
+        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+        <script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[6]='MMERGE6';ftypes[6]='phone';fnames[3]='MMERGE3';ftypes[3]='dropdown';fnames[4]='MMERGE4';ftypes[4]='dropdown';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
         <div className="container">
           <Logo
             clsColor1="#c47200"
